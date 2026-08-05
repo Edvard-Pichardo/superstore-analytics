2d31d96be5640da51e239e4dcfa4c92d80058614
EdvardP <magnetoseta5@gmail.com>
2026-08-04 20:08:21 -0600

    Se actualizó el documento para verificar que todos los datos se hayan añadido

--- sql/dml/05_load_clean_data.sql
+++ sql/dml/05_load_clean_data.sql
@@ -115,3 +115,148 @@ FROM clean_sales
 ORDER BY row_id
 LIMIT 10;
 
+
+
+-- Validamos la carga tipada
+-- Comparamos la cantidad total de registros entre ambas tablas.
+SELECT
+    (SELECT COUNT(*) FROM stg_sales)
+        AS registros_staging,
+
+    (SELECT COUNT(*) FROM clean_sales)
+        AS registros_clean,
+
+    (
+        (SELECT COUNT(*) FROM stg_sales)
+        -
+        (SELECT COUNT(*) FROM clean_sales)
+    ) AS diferencia_registros;
+
+
+-- Comparamos los valores faltantes antes y después
+-- de realizar la conversión de tipos.
+SELECT
+    (SELECT SUM(ship_mode IS NULL) FROM stg_sales)
+        AS ship_mode_null_staging,
+
+    (SELECT SUM(ship_mode IS NULL) FROM clean_sales)
+        AS ship_mode_null_clean,
+
+    (SELECT SUM(sales IS NULL) FROM stg_sales)
+        AS sales_null_staging,
+
+    (SELECT SUM(sales IS NULL) FROM clean_sales)
+        AS sales_null_clean,
+
+    (SELECT SUM(quantity IS NULL) FROM stg_sales)
+        AS quantity_null_staging,
+
+    (SELECT SUM(quantity IS NULL) FROM clean_sales)
+        AS quantity_null_clean,
+
+    (SELECT SUM(profit IS NULL) FROM stg_sales)
+        AS profit_null_staging,
+
+    (SELECT SUM(profit IS NULL) FROM clean_sales)
+        AS profit_null_clean;
+
+
+-- Comparamos los totales numéricos usando la misma precisión
+-- definida en la tabla clean_sales.
+SELECT
+    (
+        SELECT SUM(CAST(sales AS DECIMAL(15, 6)))
+        FROM stg_sales
+    ) AS sales_staging,
+
+    (
+        SELECT SUM(sales)
+        FROM clean_sales
+    ) AS sales_clean,
+
+    (
+        SELECT SUM(
+            CAST(
+                CAST(quantity AS DECIMAL(20, 6))
+                AS UNSIGNED
+            )
+        )
+        FROM stg_sales
+    ) AS quantity_staging,
+
+    (
+        SELECT SUM(quantity)
+        FROM clean_sales
+    ) AS quantity_clean,
+
+    (
+        SELECT SUM(CAST(discount AS DECIMAL(4, 2)))
+        FROM stg_sales
+    ) AS discount_staging,
+
+    (
+        SELECT SUM(discount)
+        FROM clean_sales
+    ) AS discount_clean,
+
+    (
+        SELECT SUM(CAST(profit AS DECIMAL(15, 6)))
+        FROM stg_sales
+    ) AS profit_staging,
+
+    (
+        SELECT SUM(profit)
+        FROM clean_sales
+    ) AS profit_clean;
+
+
+
+-- Calculamos la diferencia entre los totales de ambas tablas.
+-- Todos los resultados deben ser cero.
+SELECT
+    (
+        SELECT SUM(CAST(sales AS DECIMAL(15, 6)))
+        FROM stg_sales
+    )
+    -
+    (
+        SELECT SUM(sales)
+        FROM clean_sales
+    ) AS diferencia_sales,
+
+    (
+        SELECT SUM(
+            CAST(
+                CAST(quantity AS DECIMAL(20, 6))
+                AS UNSIGNED
+            )
+        )
+        FROM stg_sales
+    )
+    -
+    (
+        SELECT SUM(quantity)
+        FROM clean_sales
+    ) AS diferencia_quantity,
+
+    (
+        SELECT SUM(CAST(discount AS DECIMAL(4, 2)))
+        FROM stg_sales
+    )
+    -
+    (
+        SELECT SUM(discount)
+        FROM clean_sales
+    ) AS diferencia_discount,
+
+    (
+        SELECT SUM(CAST(profit AS DECIMAL(15, 6)))
+        FROM stg_sales
+    )
+    -
+    (
+        SELECT SUM(profit)
+        FROM clean_sales
+    ) AS diferencia_profit;
+
+
